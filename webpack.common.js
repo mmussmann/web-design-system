504333554c9756b6dee89736f60aaad74a3c7c16
Martin Mussmann <MAMA@milestone.dk>
2019-06-24 14:58:44 +0200

    Fixed eslint config

--- webpack.common.js
+++ webpack.common.js
@@ -23,12 +23,10 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          fix: true
-        }
+        loader: 'eslint-loader'
       },
       {
         test: /\.js$/,
