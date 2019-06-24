12f879d10af4be392f8ae201aee636e4d1dbfbb9
Martin Mussmann <MAMA@milestone.dk>
2019-06-24 09:08:39 +0200

    added npm prodider

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const FractalWebpackPlugin = require('fractal-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -37,6 +38,11 @@ module.exports = merge(common, {
       new FractalWebpackPlugin({
         mode: 'build',
         configPath: 'fractal.js'
-      })
+      }),
+      new CopyPlugin([
+        { from: 'package.json', to: 'package.json'},
+        { from: 'LICENSE', to: 'LICENSE', toType: 'file'},
+        { from: 'README.md', to: 'README.md'},
+      ])
     ]
 });
