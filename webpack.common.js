5621bb94879b7bc8937f817ccae3f89b40d21134
Martin Mussmann <MAMA@milestone.dk>
2019-06-24 14:27:55 +0200

    eslint with standard

--- webpack.common.js
+++ webpack.common.js
@@ -1,37 +1,45 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    resolve: {
-        alias: {
-            components: path.resolve(__dirname, './components'),
-            scss: path.resolve(__dirname, './src/scss')
-        }
-    },
-    entry: {
-        main: path.resolve('./src/js/main.js')
-    },
-    output: {
-        path: path.resolve('./public/'),
-        filename: '[name].js',
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, './components'),
+      scss: path.resolve(__dirname, './src/scss')
     }
-};
+  },
+  entry: {
+    main: path.resolve('./src/js/main.js')
+  },
+  output: {
+    path: path.resolve('./public/'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
