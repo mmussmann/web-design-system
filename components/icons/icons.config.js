61dee91049d201d247db41854f4bb6351f752ea8
Martin Mussmann <MAMA@milestone.dk>
2019-08-21 11:40:08 +0200

    Cleanu

--- components/icons/icons.config.js
+++ components/icons/icons.config.js
@@ -2,7 +2,11 @@ module.exports = {
   status: 'ready',
   title: 'Available icons',
   context: {
-    inlineCss: 'svg{height: 50px; width: 50px;margin: 5px}'
+    previewCss: `svg {
+      height: 50px; 
+      width: 50px;
+      margin: 5px;
+    }`
   },
   notes: 'Overview of available icons'
 }
