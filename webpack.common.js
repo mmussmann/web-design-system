b78202b34f1e1c2ffdd710481487013a17858c5e
pjzmilestone <52495068+pjzmilestone@users.noreply.github.com>
2019-11-20 16:04:00 +0100

    added custom options fro svg optimizer to fix styles and animmations. Fixed issue with circular reloading

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {  CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const StyleLintPlugin = require('stylelint-webpack-plugin')
 const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin')
@@ -20,7 +20,7 @@ module.exports = {
     filename: '[name].js'
   },
   plugins: [
-    new CleanWebpackPlugin({
+    new CleanWebpackPlugin({ 
       cleanAfterEveryBuildPatterns: ['!theme-overrides/**/*', '!src/**/*']
     }),
     new StyleLintPlugin({
@@ -31,7 +31,59 @@ module.exports = {
     new SVGSpritemapPlugin('src/svg/**/*.svg', {
       output: {
         filename: 'msds-spritemap.svg',
-        svgo: false,
+        svgo: {
+          plugins: [
+            { cleanupAttrs: true },
+            { inlineStyles: true },
+            { removeDoctype: true },
+            { removeXMLProcInst: true },
+            { removeComments: true },
+            { removeMetadata: true },
+            { removeTitle: true },
+            { removeDesc: true },
+            { removeUselessDefs: true },
+            { removeXMLNS: false },
+            { removeEditorsNSData: true },
+            { removeEmptyAttrs: true },
+            { removeHiddenElems: true },
+            { removeEmptyText: true },
+            { removeEmptyContainers: true },
+            { removeViewBox: true },
+            { cleanupEnableBackground: true },
+            { minifyStyles: false },
+            { convertStyleToAttrs: true },
+            { convertColors: true },
+            { convertPathData: false },
+            { convertTransform: true },
+            { removeUnknownsAndDefaults: true },
+            { removeNonInheritableGroupAttrs: true },
+            { removeUselessStrokeAndFill: true },
+            { removeUnusedNS: true },
+            { prefixIds: false },
+            { cleanupIDs: false },
+            { cleanupNumericValues: true },
+            { cleanupListOfValues: false },
+            { moveElemsAttrsToGroup: true },
+            { moveGroupAttrsToElems: true },
+            { collapseGroups: true },
+            { removeRasterImages: false },
+            { mergePaths: true },
+            { convertShapeToPath: true },
+            { convertEllipseToCircle: true },
+            { sortAttrs: false },
+            { sortDefsChildren: true },
+            { removeDimensions: false },
+            { removeAttrs: false },
+            { removeAttributesBySelector: false },
+            { removeElementsByAttr: false },
+            { addClassesToSVGElement: false },
+            { addAttributesToSVGElement: false },
+            { removeOffCanvasPaths: false },
+            { removeStyleElement: false },
+            { removeScriptElement: false },
+            { reusePaths: false }
+          ]
+        },
         svg: {
           sizes: false
         }
@@ -39,10 +91,10 @@ module.exports = {
       sprite: {
         prefix: false,
       },
-      styles: {
+      styles: { 
         format: 'data',
         filename: path.join(__dirname, 'src/scss/_sprites.scss'),
-        variables : {
+        variables : { 
           sizes: 'sprite-sizes'
         }
       }
@@ -55,7 +107,7 @@ module.exports = {
   ],
   module: {
     rules: [
-      {
+      { 
         enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
