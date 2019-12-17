b53124a246a54ec9fedc99054e41f4207761858d
Danny Olsen <dao@milestone.dk>
2019-12-17 14:28:44 +0100

    reverted js from solution card to category card

--- components/button-cards/_category-card/category-card.js
+++ components/button-cards/_category-card/category-card.js
@@ -2,11 +2,6 @@ window.addEventListener('load', function() {
   const cardRows = document.querySelectorAll('.card-row')
 
   cardRows.ensureCardContentsSameHeightWithinParent({
-    subSelectors: [
-      '.msds-solution-card__header-headline',
-      '.msds-solution-card__body-headline',
-      '.msds-solution-card__body-description',
-      '.msds-solution-card__footer-buttons'
-    ]
+    subSelectors: ['.msds-category-card__header-body', '.msds-category-card__content-body']
   })
 })
