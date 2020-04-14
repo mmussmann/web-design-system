a9772f0b77759a0ebea69b46f13dcb7488176674
Danny Olsen <dao@milestone.dk>
2020-04-14 11:29:32 +0200

    updated first half of the code review

--- components/forms/select-input/select-input.js
+++ components/forms/select-input/select-input.js
@@ -4,13 +4,13 @@ function SelectInput(id, options, validationMsg, placeholder, isRequired) {
   let validationMsgElement
   let isOpen = false
   let displayValidationMsg = false
+  let _valid = false
+  let _value = 'not set'
+  let _selectedOptionIndex = 0
+  let shouldPopulateList = true
   const _options = options
   const _validationMsg = validationMsg
   const _placeholder = isRequired ? placeholder + '*' : placeholder
-  let _valid = false
-  let _value = 'not set'
-  let _index = 0
-  let shouldPopulateList = true
 
   const cacheDom = function() {
     selectInput = document.getElementById(id)
@@ -23,24 +23,24 @@ function SelectInput(id, options, validationMsg, placeholder, isRequired) {
   }
 
   const render = function() {
-    _index = selectElement.value
+    _selectedOptionIndex = selectElement.value
     validationMsgElement.innerHTML = _validationMsg
     if (isOpen) {
       selectInput.classList.toggle('msds-select-input--open')
     }
     if (_validationMsg != '' && displayValidationMsg == true) {
-      validationMsgElement.style.display = 'block'
+      validationMsgElement.classList.add('msds-select-input__validation-msg--display')
     } else {
-      validationMsgElement.style.display = 'none'
+      validationMsgElement.classList.remove('msds-select-input__validation-msg--display')
     }
-    if (_index == 0) {
+    if (_selectedOptionIndex == 0) {
       selectElement.classList.add('msds-select-input__select--no-selection')
     } else {
       selectElement.classList.remove('msds-select-input__select--no-selection')
       displayValidationMsg = false
     }
     if (isEmpty(_options)) {
-      _index = 0
+      _selectedOptionIndex = 0
       return
     } else if (shouldPopulateList) {
       shouldPopulateList = false
@@ -56,25 +56,6 @@ function SelectInput(id, options, validationMsg, placeholder, isRequired) {
     _value = selectElement.options[selectElement.selectedIndex].text
   }
 
-  this.toogleOpen = function() {
-    if (!isOpen) {
-      isOpen = true
-    } else {
-      isOpen = false
-    }
-  }
-
-  this.validateComponent = function() {
-    if (_index != 0) {
-      _valid = true
-      displayValidationMsg = false
-      render()
-    } else {
-      _valid = false
-      displayValidationMsg = true
-      render()
-    }
-  }
   const createOptionElement = function(text, value, isPlaceholder) {
     const optionElement = document.createElement('option')
     optionElement.text = text
@@ -99,6 +80,25 @@ function SelectInput(id, options, validationMsg, placeholder, isRequired) {
     render()
   }
 
+  this.toggleOpen = function() {
+    if (!isOpen) {
+      isOpen = true
+    } else {
+      isOpen = false
+    }
+  }
+
+  this.validateComponent = function() {
+    if (_selectedOptionIndex != 0) {
+      _valid = true
+      displayValidationMsg = false
+      render()
+    } else {
+      _valid = false
+      displayValidationMsg = true
+      render()
+    }
+  }
   this.isValid = function() {
     if (_valid) {
       displayValidationMsg = false
