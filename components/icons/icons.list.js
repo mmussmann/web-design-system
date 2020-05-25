1a9a94731a95b8485bfbccf67c8321eefb1c92fa
Danny Olsen <dao@milestone.dk>
2020-05-25 13:50:26 +0200

    added marketplace interface

--- components/icons/icons.list.js
+++ components/icons/icons.list.js
@@ -229,5 +229,6 @@ export const richIcons = [
   { name: 'Integrator Searching male', value: 'msds-integrator-searching-male' },
   { name: 'Investigation', value: 'msds-investigation' },
   { name: 'LPR', value: 'msds-lpr' },
-  { name: 'Marketplace Filter', value: 'msds-marketplace-filter' }
+  { name: 'Marketplace Filter', value: 'msds-marketplace-filter' },
+  { name: 'Marketplace Interface', value: 'msds-marketplace-interface' }
 ]
