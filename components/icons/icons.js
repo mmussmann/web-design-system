c51c3b598bbcbea56e117b8571b58c39221dc204
pjzmilestone <52495068+pjzmilestone@users.noreply.github.com>
2020-08-25 15:28:10 +0200

    added icon files, extended existing solutions with marketplace categories, resized icons for presentation purpose

--- components/icons/icons.js
+++ components/icons/icons.js
@@ -1,10 +1,14 @@
 export function appendIconElementToContainer(iconsContainer, icon, isButtonIcon, path) {
-  const iconElement = isButtonIcon ? createButtonIconElement(icon, path) : createIconElement(icon)
+  const iconElement = isButtonIcon ? createFlatIconElement(icon, path) : createRichIconElement(icon)
   const iconWrapper = createBootstrapWrapper()
   const iconName = iconElement.getAttribute('title')
-  const iconNameElement = document.createElement('p')
+  const iconNameElement = document.createElement('div')
 
   iconNameElement.innerHTML = iconName
+  iconNameElement.classList.add('msds-text-header-5')
+  iconNameElement.classList.add('msds-text-gray-9')
+  iconNameElement.classList.add('msds-icon-title')
+
   iconsContainer.appendChild(iconWrapper)
   iconWrapper.appendChild(iconElement)
   iconWrapper.appendChild(iconNameElement)
@@ -12,26 +16,28 @@ export function appendIconElementToContainer(iconsContainer, icon, isButtonIcon,
 
 function createBootstrapWrapper() {
   const iconWrapperElement = document.createElement('div')
-  iconWrapperElement.classList.add('msds-icon')
+  iconWrapperElement.classList.add('msds-icon-wrapper')
 
   return iconWrapperElement
 }
 
-function createIconElement(icon) {
+function createRichIconElement(icon) {
   const iconElement = document.createElement('div')
 
   iconElement.classList.add(icon.value)
+  iconElement.classList.add('msds-rich-icon')
   iconElement.setAttribute('title', icon.name)
 
   return iconElement
 }
 
-function createButtonIconElement(icon, path) {
+function createFlatIconElement(icon, path) {
   const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
   const useElement = document.createElementNS('http://www.w3.org/2000/svg', 'use')
 
   useElement.setAttribute('href', `${path}${icon.previewValue}`)
   svgElement.setAttribute('title', icon.name)
+  svgElement.classList.add('msds-flat-icon')
 
   svgElement.appendChild(useElement)
 
--- components/icons/rich-icons.js
+++ components/icons/rich-icons.js
@@ -2,12 +2,23 @@ import { richIcons } from './icons.list'
 import { appendIconElementToContainer } from './icons.js'
 
 function init() {
-  const iconsContainer = document.querySelector('.msds-rich-icons')
+  const genericIconsContainer = document.querySelector('.msds-generic-rich-icons')
+  const marketplaceIconsContainer = document.querySelector('.msds-marketplace-rich-icons')
 
-  if (iconsContainer) {
-    richIcons.sort((iconA, iconB) => (iconA.name > iconB.name ? 1 : -1))
+  if (genericIconsContainer) {
+    const genericIcons = richIcons
+      .filter(icon => !icon.name.startsWith('Marketplace '))
+      .sort((iconA, iconB) => (iconA.name > iconB.name ? 1 : -1))
 
-    richIcons.forEach(icon => appendIconElementToContainer(iconsContainer, icon))
+    genericIcons.forEach(icon => appendIconElementToContainer(genericIconsContainer, icon))
+  }
+
+  if (marketplaceIconsContainer) {
+    const marketplaceIcons = richIcons
+      .filter(icon => icon.name.startsWith('Marketplace '))
+      .sort((iconA, iconB) => (iconA.name > iconB.name ? 1 : -1))
+
+    marketplaceIcons.forEach(icon => appendIconElementToContainer(marketplaceIconsContainer, icon))
   }
 }
 
